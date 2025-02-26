645ce6138710b853fcef5eb7065290fad24fd6d4
Gopal Dhakal <57512065+gopaldhakal@users.noreply.github.com>
2025-02-26 10:57:57 +0545

    Update api.js for online deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://petshopbackend-nrbh.onrender.com/api";
 
 export const fetchBreeds = async () => {
   try {
@@ -22,20 +22,6 @@ export const fetchProducts = async () => {
   }
 };
 
-// export const fetchBlogs = async () => {
-//   const token = localStorage.getItem("token");
-//   try {
-//     const response = await axios.get(`${API_URL}/blogs`,
-//       {
-//       headers: { Authorization: `Bearer ${token}` },
-//     }
-//   );
-//     return response.data;
-//   } catch (error) {
-//     console.error("Error fetching blogs:", error);
-//     throw error;
-//   }
-// };
 export const fetchBlogs = async () => {
   try {
     const response = await axios.get(`${API_URL}/blogs`);
@@ -48,18 +34,10 @@ export const fetchBlogs = async () => {
 
 export const fetchSubscriptions = async () => {
   try {
-    const response = await axios.get("http://localhost:5000/api/subscriptions");
+    const response = await axios.get(`${API_URL}/subscriptions`);
     return response.data;
   } catch (error) {
     console.error("Error fetching subscriptions:", error);
     throw error;
   }
 };
-
-// export const deleteSubscription = async (id) => {
-//   try {
-//     await axios.delete(`/api/subscriptions/${id}`);
-//   } catch (error) {
-//     console.error("Error deleting subscription:", error);
-//   }
-// };
